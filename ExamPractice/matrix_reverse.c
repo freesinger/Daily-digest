1315c97ccf8f6072f2bc67a3ea8d26a63f51d10c
ShaneWang <wzy545@vip.qq.com>
2018-01-29 21:47:20 +0800

    new file:   ExamPractice/matrix_reverse.c

---
+++ ExamPractice/matrix_reverse.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+
+int main(void)
+{
+    int N, num, temp;
+    scanf("%d", &N);
+	int s[N][N];
+    
+    for (int i = 0; i < N; i++) {
+        for (int j = 0; j < N; j++) {
+            scanf("%d", &s[i][j]);
+        }
+    }
+    
+    for (int i = 0; i < N; i++) {
+        for (int j = 0; j < i; j++) {
+            temp = s[i][j];
+            s[i][j] = s[j][i];
+            s[j][i] = temp;
+        }
+    } 
+    
+    for (int i = 0; i < N; i++) {
+        for (int j = 0; j < N; j++)
+			printf("%d ", s[i][j]);
+    	printf("\n");
+    }
+    return 0;
+}
